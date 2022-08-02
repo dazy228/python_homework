fb3c2c02b7a93b6f995fd02b72e886e89c378ceb
ПК <danilchupak@gmail.com>
2022-08-02 23:29:02 +0300

    homework done

---
+++ main.py
@@ -0,0 +1,6 @@
+common_string = str(input("Введите какой либо текст ", ))
+even_line = common_string[::2]
+upper_line = common_string[-1::-1].upper()
+print(even_line)
+print(upper_line)
+# My first project on the PyCharm! - the text I enter^)

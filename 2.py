cc7e979fafd8552d93740f40577f77c4a77b011f
ПК <danilchupak@gmail.com>
2022-08-05 01:22:38 +0300

    add 2.py

--- 2.py
+++ 2.py
@@ -1,7 +1,30 @@
-first = [1, 2, 3, 4]
-a1 = first
-a2 = first
-a3 = first
+a1 = "123"
+a2 = "123"
+a3 = "123"
+print(a1, a2, a3)
+print(a1 == a2 == a3)
+print(id(a1))
+print(id(a2))
+print(id(a3))
 
-a1 == a2
-a2 == a3
+a1 = int
+a3 = float
+print(a1 == a2 == a3)
+print(id(a1))
+print(id(a2))
+print(id(a3))
+print("------------------------------------------------------")
+b1 = 321
+b2 = "321"
+print(b1, b2)
+print(b1 == b2)
+print(id(b1))
+print(id(b2))
+b1 = bool
+b2 = bool
+print(b1, b2)
+print(b1 == b2)
+print(id(b1))
+print(id(b2))
+
+

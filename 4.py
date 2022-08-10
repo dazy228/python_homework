4d252181ece3ce14462f2829043a74315246a78b
ПК <danilchupak@gmail.com>
2022-08-10 23:24:07 +0300

    easy!

--- 4.py
+++ 4.py
@@ -1,7 +1,6 @@
-
 while True:
     name = input("Введите своё имя: ", ).title()
-    if not name.isdigit():
+    if name.isalpha():
         break
     else:
         print("Имя не может состоять из цифр, попробуйте ещё раз!")
@@ -12,20 +11,17 @@ while True:
         print("Ошибка, повторите ввод!")
     elif int(age) < 10:
         print(f"Привет, шкет {name}")
-        print("---------------------")
-        answer = input("Хотите выйти? (y/д): ")
-        if answer in ("y", "д", "Y", "Д"):
-            break
-        else:
-            continue
+        print("-------------------------")
+
     elif int(age) <= 18:
         print(f"Как жизнь {name}?")
-        print("---------------------")
-        answer = input("Хотите выйти? (y/д): ")
-        if answer in ("y", "д", "Y", "Д"):
-            break
-        else:
-            continue
+        print("-------------------------")
+    elif int(age) < 100:
+        print(f"Что желаете {name}?")
+        print("-------------------------")
     else:
         print(f"{name}, вы лжете - в наше время столько не живут...")
-
+        print("-------------------------")
+    answer = input(f"{name}, желаете выйти? (Д/Y) - ").upper()
+    if answer in ("Д", "Y"):
+        break

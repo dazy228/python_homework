be1dc8f95600314debf95ab776465a652c657088
ПК <danilchupak@gmail.com>
2022-08-11 00:27:13 +0300

    homework number 5 done!?!?!?!

---
+++ 5.py
@@ -0,0 +1,25 @@
+value = int(input("Введите целое число: "))
+result = 0
+i = 1
+item = 1
+print("-----method for-----")
+# su = sum([i*i*i for i in range(1, n+1)])
+# print("Result: ", su)
+# --------------------------------------
+for item in range(1, value+1):
+    i = item**3
+    if item % 3 == 0:
+        continue
+    result += i
+print(f"Ответ: {result}")
+
+print("----method while----")
+while i <= value:
+    if i % 3 == 0:
+        continue
+    item = i**3
+    print(item)
+    i += 1
+    print(result)
+print(f"Ответ: {result}")
+

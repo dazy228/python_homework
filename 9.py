900909a1cffe62fc42bef068905ea00eb5b02136
ПК <danilchupak@gmail.com>
2022-08-13 03:55:40 +0300

    homework number 9 done!

--- 9.py
+++ 9.py
@@ -3,9 +3,9 @@ old_dictionary = {"a": 29, "b": 63, "c": 18, "d": 21, "e": 54}
 
 def dict_swap():
     new_dictionary = {value: key for key, value in old_dictionary.items()}
-    return new_dictionary
+    print(f"old dictionary: {old_dictionary}")
+    print("-" * 61)
+    print(f"new dictionary: {new_dictionary}")
 
 
-print(dict_swap())
-
-
+dict_swap()

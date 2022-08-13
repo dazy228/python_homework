e1972fea4d20a65dd3468bf00df2c23044a7cd12
ПК <danilchupak@gmail.com>
2022-08-13 03:37:56 +0300

    homework number 6 done!

---
+++ 6.py
@@ -0,0 +1,43 @@
+import random
+
+
+def condition_set():
+    if number < random_num:
+        print("Твоё число меньше того, что я загадал:)")
+    if number > random_num:
+        print("Твоё число больше загаданного!")
+
+
+def you_win():
+    if number == random_num:
+        print("-" * 50)
+        print(f"Ух ты, {name}! Ты угадал число использовав кол-во попыток: {number_of_attempts}!")
+        print("-" * 50)
+
+
+def you_lose():
+    if number != random_num:
+        print("-" * 50)
+        print(f"А вот и не угадал:( Я загадал число: {random_num}(((")
+        print("-" * 50)
+
+
+def welcome_to_my_game():
+    print(f"Отлично, {name}, я загадал число между 1 и 30. Сможешь угадать?:)")
+    print("У тебя есть 6 попыток, погнали!")
+
+
+name = input("Привет! Как тебя зовут? ").title()
+number_of_attempts = 0
+random_num = random.randint(1, 30)
+
+welcome_to_my_game()
+
+while number_of_attempts < 6:
+    number = int(input("Введите число: "))
+    number_of_attempts += 1
+    condition_set()
+    if number == random_num:
+        break
+you_win()
+you_lose()

0a760ab7d2296f6017ce61dd13fcc19961c3fe5b
ПК <danilchupak@gmail.com>
2022-08-17 22:56:11 +0300

    homework number 6 done!

--- 6.py
+++ 6.py
@@ -21,7 +21,7 @@ def you_win():
 
 
 def you_lose():
-    if number_of_attempts >= 6:
+    if number_of_attempts > 6:
         print("-" * 50)
         print(f"А вот и не угадал:( Я загадал число: {random_num}(((")
         print("-" * 50)

cc40808b1a31bf4e668799baca2cde877fde2867
ПК <danilchupak@gmail.com>
2022-08-19 23:58:53 +0300

    homework number 11 done!

---
+++ 11.py
@@ -0,0 +1,22 @@
+def func_input_data_lower():
+    for word in input_data:
+        word_lower = word.lower()
+        input_data_lower.append(word_lower)
+
+
+def filter_list():
+    for word in input_data_lower:
+        if word == word[-1::-1]:
+            words.append(word)
+        else:
+            pass
+
+
+input_data = ['Страна', 'шалаш', 'Летел', 'вертолёт', 'УЧУ', 'мэм', 'язык']  # В условии когда писали список слов допустили синтаксическую ошибку. не inputdata а input_data^)
+words = []
+input_data_lower = []
+
+func_input_data_lower()
+
+result = list(filter(filter_list(), words))
+print(result)

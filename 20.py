d881e370b3ae35f4f7fdc3e8db9e82010b8873ac
ПК <danilchupak@gmail.com>
2022-09-16 02:25:20 +0300

    homework 20 done!

--- 20.py
+++ 20.py
@@ -1,33 +1,5 @@
 import time
-
-
-class Auto(object):
-    brand = 'not specified'
-    age = 'not specified'
-    color = 'not specified'
-    mark = 'not specified'
-    weight = 'not specified'
-
-    def __init__(self, brand, age, mark, color='black', weight=1.73):
-        self.brand = brand
-        self.age = age
-        self.mark = mark
-        self.color = color
-        self.weight = weight
-
-    def move(self):
-        print('move')
-
-    def birthday(self, age):
-        self.age = age
-        age += 1
-        print(f'{age}')
-
-    def stop(self):
-        print('stop')
-
-
-auto = Auto('BMW', 12, 'BMW')
+from home_work19 import Auto
 
 
 class Truck(Auto):
--- home_work19.py
+++ home_work19.py
@@ -1,10 +1,4 @@
 class Auto(object):
-    brand = 'not specified'
-    age = 'not specified'
-    color = 'not specified'
-    mark = 'not specified'
-    weight = 'not specified'
-
     def __init__(self, brand, age, mark, color='black', weight=1.73):
         self.brand = brand
         self.age = age
@@ -25,6 +19,3 @@ class Auto(object):
 
 
 auto = Auto('BMW', 12, 'BMW')
-auto.stop()
-auto.move()
-auto.birthday(0)

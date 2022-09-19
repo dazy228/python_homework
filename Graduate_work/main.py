f2860ef212897da5ccf1e065fbce448cc300943d
ПК <danilchupak@gmail.com>
2022-09-20 02:04:06 +0300

    Graduate work DONE!

--- Graduate_work/main.py
+++ Graduate_work/main.py
@@ -1,104 +1,19 @@
-# Написать программу для работы с данными о людях
-
+from person import *
 import os
-import openpyxl
-import datetime
-
-
-class Person(object):
-    def __init__(self, name, surname, patronymic, birthday, data_death):
-        self.name = name
-        self.surname = surname
-        self.patronymic = patronymic
-        self.birthday = birthday
-        self.data_death = data_death
-
-    def __str__(self):
-        return f'Имя: {self.name}, Фамилия: {self.surname}, Отчество: {self.patronymic}, Возраст: {"  "}, Дата рождения: {self.birthday}, Дата смерти: {self.data_death}'.title()
-
-    def get_full_name(self):
-        return f'{self.surname} {self.name} {self.patronymic}'.title()
-
-
-class ValidateDate(Person):
-    def valid_date(self):
-        birthday = self.birthday
-        valid_data_list = []
-        birthday = birthday.split('.')
-        for i in birthday:
-            valid_data_list.append(int(i))
-        return valid_data_list
-
-
-class PersonList(object):
-    def __init__(self):
-        self.persons = []
-
-    def __str__(self):
-        return f'Список людей: {self.persons}'
-
-    def __repr__(self):
-        return f'Список людей: {self.persons}'
-
-    def add_person(self, person):
-        self.persons.append(person)
-
-    def save(self):
-        wb = openpyxl.Workbook()
-        sheet = wb.active
-        sheet.title = 'Люди'
-        sheet.append(['Имя', 'Фамилия', 'Отчество', 'Дата рождения', 'Дата смерти'])
-        for person in self.persons:
-            sheet.append([person.name, person.surname, person.patronymic, person.birthday, person.data_death])
-        wb.save('persons.xlsx')
-
-    def load(self):
-        wb = openpyxl.load_workbook('persons.xlsx')
-        sheet = wb['Люди']
-        for row in sheet.iter_rows(min_row=2):
-            person = Person(row[0].value, row[1].value, row[2].value, row[3].value, row[4].value)
-            self.persons.append(person)
-
-    def get_info(self):
-        for person in self.persons:
-            print(person)
-
-    def find_persons(self, search_name):
-        persons = []
-        for person in self.persons:
-            if search_name in person.get_full_name():
-                persons.append(person)
-        return persons
-
-    def find_person(self, surname):
-        for person in self.persons:
-            if person.surname == surname:
-                return person
-        return None
-
-    def delete_person(self, surname):
-        person = self.find_person(surname)
-        if person is not None:
-            self.persons.remove(person)
-            return True
-        return False
 
 
 def main():
     person_list = PersonList()
-    if os.path.exists('persons.xlsx'):
-        person_list.load()
+
     while True:
         print('-' * 30)
-        print('1. Добавить человека')
-        print('2. Найти человека')
-        print('3. Удалить человека')
-        print('4. Сохранить')
-        print('5. Вывести базу')
-        print('6. Выйти')
+        print('1. Добавить человека', '\n2. Найти человека',
+              '\n3. Удалить человека', '\n4. Сохранить',
+              '\n5. Вывести базу', '\n6. Выйти', '\n7. Загрузить базу данных')
         print('-' * 30)
         choice = input('Выберите пункт меню: ')
         if choice == '1':
+
             while True:
                 name = input('Введите имя: ')
                 if name == '':
@@ -108,41 +23,60 @@ def main():
             surname = input('Введите фамилию: ')
             patronymic = input('Введите отчество: ')
             while True:
+                sex = input('Введите пол (М/Ж): ').upper()
+                if sex in ('М', 'Ж'):
+                    break
+                else:
+                    print("Неправильный формат")
+            while True:
                 birthday = input('Введите дату рождения: ')
                 try:
-                    birthday = datetime.datetime.strptime(birthday, '%d.%m.%Y').strftime('%d.%m.%Y')
+                    birthday = datetime.datetime.strptime(get_valid_date(birthday), '%d.%m.%Y').strftime('%d.%m.%Y')
                     break
                 except ValueError:
                     print('Неверный формат даты')
             while True:
                 data_death = input('Введите дату смерти: ')
-                try:
-                    data_death = datetime.datetime.strptime(data_death, '%d.%m.%Y').strftime('%d.%m.%Y')
+                if len(data_death) == 0:
+                    data_death = None
                     break
-                except ValueError:
-                    print('Неверный формат даты')
-            person = Person(name, surname, patronymic, birthday, data_death)
-            person_list.add_person(person)
+                else:
+                    try:
+                        data_death = datetime.datetime.strptime(get_valid_date(data_death), '%d.%m.%Y').strftime('%d.%m.%Y')
+                        break
+                    except ValueError:
+                        print('Неверный формат даты')
+            person_list.add_person(Person(name, surname, patronymic, sex, birthday, data_death))
+
         elif choice == '2':
-            search_name = input('Введите слово по которому будет произведён поиск: ')
-            person = person_list.find_persons(search_name)
+            find = input('Введите слово по которому будет произведён поиск: ')
+            person = person_list.find_persons(find)
             if person is not None:
                 for i in person:
                     print(i)
             else:
                 print('Человек не найден')
         elif choice == '3':
-            surname = input('Введите фамилию: ')
-            if person_list.delete_person(surname):
+            if person_list.delete_person(input('Введите фамилию: ')):
                 print('Человек удален')
             else:
                 print('Человек не найден')
         elif choice == '4':
-            person_list.save()
+            if input('Сохранить в новый файл? (Да/Нет):').upper() in "ДА":
+                file_name = input('Введите имя файла без расширения:')
+                person_list.save(file_name)
+            else:
+                person_list.save(person_list.file_name)
         elif choice == '5':
             person_list.get_info()
         elif choice == '6':
             break
+        elif choice == '7':
+            file_name = input('Укажите имя файла:')
+            if os.path.exists(f'{file_name}.xlsx'):
+                person_list.load(file_name)
+            else:
+                print('Такого файла нет в корне')
         else:
             print('Неверный пункт меню')
 

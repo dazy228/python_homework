c44cc94e619817cb1cb1e9f1764e7cb0ef7f547a
ПК <danilchupak@gmail.com>
2022-09-20 04:20:03 +0300

    Вот и дипломная работа)

--- Graduate_work/person.py
+++ Graduate_work/person.py
@@ -10,6 +10,7 @@ class Person(object):
         self.sex = sex
         self.birthday = birthday
         self.data_death = data_death
+        # Метод . просто метод%) ВОЛШЕБНЫЙ
 
     def get_full_name(self):
         """
@@ -17,6 +18,7 @@ class Person(object):
         :return:
         """
         return f'{self.surname} {self.name} {self.patronymic}'.lower()
+        # Метод для получения полного имени по которому потом будет производиться поиск человека в базе данных
 
     @staticmethod
     def valid_date(data):
@@ -26,12 +28,14 @@ class Person(object):
             valid_data_list.append(int(i))
         date_time = datetime.date(valid_data_list[2], valid_data_list[1], valid_data_list[0])
         return date_time
+        # СтатикМетод для перевода даты в формат datetime для дальнейшего вычисления возраста человека
 
     @staticmethod
     def valid_none(word):
         if word is None:
             return "__"
         return word
+        # СтатикМетод для проверки ввода на NoneType и замены его на '__' для корректной работы метода __str__
 
     def get_age(self):
         day_today = datetime.date.today()
@@ -42,6 +46,7 @@ class Person(object):
         delta = age.split(',')
         year = delta[0].split(' ')
         return year[0]
+        # Метод для вычисления возраста человека по дате рождения и дате смерти (если есть)
 
     def __str__(self):
         return '| Имя: {:<10} | Фамилия: {:<10} | Отчество: {:<10} | '\
@@ -49,22 +54,29 @@ class Person(object):
                'Родил{} {:^10} | Умер: {:^10}|'.format(self.name, self.valid_none(self.surname),
                                                        self.valid_none(self.patronymic), self.sex,
                                                        self.get_age(), 'cя: ' if self.sex in 'М' else 'ась:',
-                                                       self.birthday, self.valid_none(self.data_death)).title()
+                                                       self.birthday, self.valid_none(self.data_death))
+        # Делаем изумительно красивы и продуманный вывод;) Все в одну строку, но читаемо и понятно
 
 
 class PersonList(object):
+    file_name = None
+    # Присваиваем имени файла по дефолту None дабы избежать ошибки при сохранении файла
+
     def __init__(self):
         self.persons = []
-
+        # тоже, просто Метод инициализации класса
 
     def __str__(self):
         return f'Список людей: {self.persons}'
+        # Метод для вывода списка людей в строку
 
     def __repr__(self):
         return f'Список людей: {self.persons}'
+        # Метод для вывода списка людей в строку
 
     def add_person(self, person):
         self.persons.append(person)
+        # Метод для добавления человека в список
 
     def save(self, file_name):
         wb = openpyxl.Workbook()
@@ -77,6 +89,7 @@ class PersonList(object):
         wb.save(f'{file_name}.xlsx')
         wb.close()
         print("Файл сохранился")
+        # Метод для сохранения всего нашего файла в формате xlsx с кастомным или уже существующим именем
 
     def load(self, file_name):
         wb = openpyxl.load_workbook(f'{file_name}.xlsx')
@@ -87,10 +100,12 @@ class PersonList(object):
         wb.close()
         self.file_name = file_name
         print('Загружена база данных')
+        # Метод для загрузки файла с уже существующим именем
 
     def get_info(self):
         for person in self.persons:
             print(person)
+        # Метод для вывода информации о людях в консоль, напоменаем.. 'изумительно красиво и продуманно'
 
     def find_persons(self, search_name):
         persons = []
@@ -98,12 +113,14 @@ class PersonList(object):
             if search_name in person.get_full_name():
                 persons.append(person)
         return persons
+        # Метод для поиска людей по полному имени или его части
 
     def find_person(self, surname):
         for person in self.persons:
             if person.surname == surname:
                 return person
         return None
+        # Метод для поиска людей по фамилии для того же удаления
 
     def delete_person(self, surname):
         person = self.find_person(surname)
@@ -111,6 +128,7 @@ class PersonList(object):
             self.persons.remove(person)
             return True
         return False
+        # Метод для удаления человека из списка по фамилии
 
 
 def get_valid_date(input_date: str):
@@ -120,3 +138,4 @@ def get_valid_date(input_date: str):
     :return:
     """
     return input_date.replace(" ", ".", 2).replace("/", ".", 2).replace("-", ".", 2)
+    # Функция для валидации даты под метод вычисления возраста человека
